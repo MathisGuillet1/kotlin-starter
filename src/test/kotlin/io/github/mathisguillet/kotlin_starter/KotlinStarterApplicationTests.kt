bb2fe33d8c818ddd33ca4564a99f5c51ac240b75
MathisGuillet1 <mathis.guillet@outlook.com>
2024-06-10 16:38:19 +0200

    Initialize project with spring initializr

---
+++ src/test/kotlin/io/github/mathisguillet/kotlin_starter/KotlinStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.mathisguillet.kotlin_starter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinStarterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
