ced787a6c191bb9cb66b394f73ba66dbed3d9170
MathisGuillet1 <mathis.guillet@outlook.com>
2024-06-19 14:50:07 +0200

    Enhance dependency management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,17 @@
 plugins {
     id("kotlin-starter.conventions")
-    id("org.springframework.boot") version "3.3.0"
-    id("io.spring.dependency-management") version "1.1.5"
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
-    id("com.google.cloud.tools.jib") version "3.4.3"
-    kotlin("plugin.spring") version "2.0.0"
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.jib)
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
+    implementation("io.github.oshai:kotlin-logging-jvm")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 jib {
--- buildSrc/src/main/kotlin/kotlin-starter.conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-starter.conventions.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     kotlin("jvm")
     jacoco
+    id("org.jlleitschuh.gradle.ktlint")
 }
 
 group = "io.github.mathisguillet"
@@ -10,6 +11,21 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    val versionCatalog = rootProject.extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+    // Dependencies shared across all modules
+    implementation(platform(versionCatalog.findLibrary("spring-boot-dependencies").get()))
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Dependencies versioning for modules
+    constraints {
+        implementation(versionCatalog.findLibrary("kotlin-logging").get())
+    }
 }
 
 kotlin {
